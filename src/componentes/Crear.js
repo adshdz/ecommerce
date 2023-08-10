96161062665c0efa82ed2d8a3c930a701409184e
Andres Steven Hernandez Ramirez <andress.hernandezp@ecci.edu.co>
2023-08-09 20:16:50 -0500

    local storage set - get

--- src/componentes/Crear.js
+++ src/componentes/Crear.js
@@ -3,33 +3,52 @@ import React, { useState } from 'react'
 
 export const Crear = () => {
 
-  
-const [pelicula, setpelicula]= useState({});
-
+  const Titulo1 = "Añadir pelicula";
+  const [pelicula, setpelicula] = useState({});
+  const [pelititulo, setpelititulo] = useState("");
   function conseguirDatos(e) {
-  ;
+    ;
     e.preventDefault();
 
     let titulo = e.target.title.value;
     let descripcion = e.target.description.value;
-   
-      let peli = {
-        id: new Date().getTime(),
-        titulo,
-        descripcion
-      }
-      
-      setpelicula(peli);
 
-      console.log(pelicula);
+    let peli = {
+      id: new Date().getTime(),
+      titulo,
+      descripcion
+    }
+
+    setpelicula(peli);
+    setpelititulo(titulo);
+    setTimeout(() => {
+      setpelititulo("");
+    }, 20000);
+      guardarStorage(peli);
+      
   };
 
-  const Titulo = "Añadir pelicula";
+const guardarStorage = (pelis) =>{
+      let elementos = JSON.parse(localStorage.getItem("pelis"));
+
+      if(Array.isArray(elementos)){
+        elementos.push(pelis);
+      } else{
+        elementos = [pelis]}
+
+        localStorage.setItem("pelis", JSON.stringify(elementos));
+
+        console.log(elementos);
+}
 
   return (
     <div>
       <div className="add">
-        <h3 className="title">{Titulo}</h3>
+        <h3 className="title">{Titulo1}</h3>
+
+        <strong>
+          {(pelititulo.length > 1) ? "has creado la pelicula:  " + pelicula.titulo : ""}
+        </strong>
         <form onSubmit={e => conseguirDatos(e)}>
           <input
             type="text"
